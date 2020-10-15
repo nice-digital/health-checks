07ba5bc2f8e91dd1223a24b1ae3a83c7c6502c71
John Williams <jcwilliams@gmail.com>
2020-10-15 14:56:00 +0100

    STAK-18 - initial setup of healthchecksui dashboard.

--- NICE.Dashboard/Startup.cs
+++ NICE.Dashboard/Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +8,21 @@ namespace NICE.Dashboard
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		private readonly IConfiguration Configuration;
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services
+				.AddHealthChecksUI(setupSettings: setup =>
+				{
+					setup.MaximumHistoryEntriesPerEndpoint(100); //limits database size.
+				})
+				.AddSqlServerStorage(Configuration.GetConnectionString("DefaultConnection"));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,9 +37,9 @@ namespace NICE.Dashboard
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapGet("/", async context =>
+				endpoints.MapHealthChecksUI(setupOptions: setup =>
 				{
-					await context.Response.WriteAsync("Hello World!");
+					setup.UIPath = "/";
 				});
 			});
 		}
